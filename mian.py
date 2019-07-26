a1095edc7c9b20dbff6a625a8eab37610fcdfebd
ShuangYuanL <44334870+ShuangYuanL@users.noreply.github.com>
2019-07-26 12:51:52 +0800

    Add files via upload

--- mian.py
+++ mian.py
@@ -5,11 +5,12 @@ import scipy
 from scipy import ndimage
 import cnn_utils
 import cv2
-#加载数据集
+#load_dataset
 def load_dataset():
 	X_train_orig , Y_train_orig , X_test_orig , Y_test_orig , classes = cnn_utils.load_dataset()
 	return X_train_orig , Y_train_orig , X_test_orig , Y_test_orig 
-#初始化数据集
+
+#init_dataset
 def init_dataset(X_train_orig , Y_train_orig , X_test_orig , Y_test_orig ):
 	X_train = X_train_orig/255.
 	X_test = X_test_orig/255.
@@ -22,13 +23,15 @@ def init_dataset(X_train_orig , Y_train_orig , X_test_orig , Y_test_orig ):
 	print ("X_test shape: " + str(X_test.shape))
 	print ("Y_test shape: " + str(Y_test.shape))
 	return X_train, Y_train, X_test, Y_test
-#创建占位符
+
+#create_placeholder
 def create_placeholder(n_H0, n_W0, n_C0, n_y):
 	X = tf.placeholder(tf.float32, [None, n_H0, n_W0, n_C0], name = "X")
 	# X = tf.placeholder(tf.float32, [None, n_H0*n_W0*n_C0], name = "X")
 	Y = tf.placeholder(tf.float32, [None,n_y], name = "Y")
 	return X,Y
-#初始化参数
+
+#init_parameters
 def init_parameters():
 	tf.set_random_seed(1) #指定随机种子
 	W1 = tf.get_variable("W1",[5,5,3,6], initializer=tf.contrib.layers.xavier_initializer(seed=0))
@@ -44,7 +47,8 @@ def init_parameters():
 	parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2, }#"W3": W3, "b3": b3, "W4": W4, "b4": b4, "W5": W5, "b5": b5} 
 
 	return parameters
-#前向传播
+
+#forward_propagation
 def forward_propagation(X, parameters,is_train_or_prediction):
 	with tf.name_scope('forward'):
 		W1 = parameters['W1'] 
@@ -100,11 +104,13 @@ def forward_propagation(X, parameters,is_train_or_prediction):
 			Z5 = tf.contrib.layers.fully_connected(Z4,6,activation_fn=None)
 
 		return Z5
-#计算loss
+
+#compute the loss
 def compute_loss(Z5,Y):
 	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = Z5, labels = Y))
 	return loss 
-#定义模型
+
+#define the model
 def model(X_train,Y_train,X_test,Y_test,learning_rate,num_epochs,minibatch_size,print_cost,isPlot):
 	seed = 3
 	(m , n_H0, n_W0, n_C0) = X_train.shape
@@ -146,8 +152,8 @@ def model(X_train,Y_train,X_test,Y_test,learning_rate,num_epochs,minibatch_size,
 			#是否打印成本
 			if print_cost:
 				#每5代打印一次
-				if epoch % 100 == 0:
-					print("当前是第 " + str(epoch) + " 代，成本值为：" + str(minibatch_cost))
+				if epoch % 10 == 0:
+					print("epoch " + str(epoch) + "costs:" + str(minibatch_cost))
 
 			#记录成本
 			if epoch % 1 == 0:
@@ -162,7 +168,6 @@ def model(X_train,Y_train,X_test,Y_test,learning_rate,num_epochs,minibatch_size,
 			plt.show()
 		#保存学习后的参数
 		parameters = sess.run(parameters)
-		print("参数已经保存到session。")
 		saver.save(sess,"model/save_net.ckpt")
 
 		#开始预测数据
@@ -185,12 +190,8 @@ def model(X_train,Y_train,X_test,Y_test,learning_rate,num_epochs,minibatch_size,
 
 		return parameters
 
-if __name__ == '__main__':
-	X_train_orig , Y_train_orig , X_test_orig , Y_test_orig  = load_dataset()
-	X_train,Y_train,X_test,Y_test = init_dataset(X_train_orig , Y_train_orig , X_test_orig , Y_test_orig)
-	parameters = model(X_train,Y_train,X_test,Y_test,learning_rate = 0.001,num_epochs = 200,minibatch_size=64,print_cost=True,isPlot=True)
-
-def load_parameters_1():
+#predict
+def predict():
 	X,_ = create_placeholder(64, 64, 3, 6)
 
 	parameters = init_parameters()
@@ -204,35 +205,58 @@ def load_parameters_1():
 	with tf.Session() as sess:
 		sess.run(init)
 		saver.restore(sess,tf.train.latest_checkpoint("model/"))
-		cap = cv2.VideoCapture(0)
-		num = 0
-		y = x = 128
-		while (1):
-			ret, frame = cap.read()
-			cv2.namedWindow("capture")
-			cv2.imshow("capture", frame)
-			k = cv2.waitKey(1) & 0xFF
-			if  k == ord('s'):
-				frame = cv2.resize(frame, (int(256), int(256)))
-				cv2.imwrite("cam/" + str(num)+".jpg", frame)
 
-				# my_image = "cam/" + str(num) + ".jpg"	
-				# num_px = 64
-				# fname =  my_image 
-				# image = np.array(ndimage.imread(fname, flatten=False))#.astype(np.float32)
-				# my_predicted_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1,64,64,3))/255
-				# my_predicted_image = my_predicted_image.astype(np.float32)
+		#use the sample picture to predict the unm
+		if (sample):
+			num = 0
+			my_image = "sample/" + str(num) + ".jpg"	
+			num_px = 64
+			fname =  my_image 
+			image = np.array(ndimage.imread(fname, flatten=False))#.astype(np.float32)
+			my_predicted_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1,64,64,3))/255
+			my_predicted_image = my_predicted_image.astype(np.float32)
 
-				# my_predicted_image = sess.run(Z5, feed_dict={X:my_predicted_image})
+			my_predicted_image = sess.run(Z5, feed_dict={X:my_predicted_image})
 
-				# plt.imshow(image) 
-				# print("预测结果: y = " + str(np.squeeze(my_predicted_image)))
-				# plt.show()
-				num = num + 1
-			elif k == ord('q'):
-				break
-		cap.release()
-		cv2.destroyAllWindows()
-# load_parameters_1()
+			plt.imshow(image) 
+			print("prediction num is : y = " + str(np.squeeze(my_predicted_image)))
+			plt.show()
+			num = num + 1
+		else:# use the camera to predict the num
+			cap = cv2.VideoCapture(0)
+			while (1):
+				ret, frame = cap.read()
+				cv2.namedWindow("capture")
+				cv2.imshow("capture", frame)
+				k = cv2.waitKey(1) & 0xFF
+				if  k == ord('s'):
+					frame = cv2.resize(frame, (int(256), int(256)))
+					cv2.imwrite("sample/cam/" + str(num)+".jpg", frame)
+
+					my_image = "sample/cam/" + str(num) + ".jpg"	
+					num_px = 64
+					fname =  my_image 
+					image = np.array(ndimage.imread(fname, flatten=False))#.astype(np.float32)
+					my_predicted_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1,64,64,3))/255
+					my_predicted_image = my_predicted_image.astype(np.float32)
+
+					my_predicted_image = sess.run(Z5, feed_dict={X:my_predicted_image})
+
+					plt.imshow(image) 
+					print("预测结果: y = " + str(np.squeeze(my_predicted_image)))
+					plt.show()
+					num = num + 1
+				elif k == ord('q'):
+					break
+			cap.release()
+			cv2.destroyAllWindows()
+
+train = 1
+if train:
+	X_train_orig , Y_train_orig , X_test_orig , Y_test_orig  = load_dataset()
+	X_train,Y_train,X_test,Y_test = init_dataset(X_train_orig , Y_train_orig , X_test_orig , Y_test_orig)
+	parameters = model(X_train,Y_train,X_test,Y_test,learning_rate = 0.001,num_epochs = 200,minibatch_size=64,print_cost=True,isPlot=True)
+else:
+	predict()
 
 
